36a8c37e4861b99687e8570c9a14a71b5522dac6
Gwen Bradbury <charmed_89@msn.com>
2021-04-16 15:07:45 +0000

    Added Category Filtering

--- gins/views.py
+++ gins/views.py
@@ -6,15 +6,21 @@ from django.shortcuts import (
     )
 from django.contrib import messages
 from django.db.models import Q
-from .models import Gin
+from .models import Gin, GinCategory
 
 
 def all_gins(request):
     """ Shows All Gins, Including Searching and Sorting Queries """
     gins = Gin.objects.all()
     query = None
+    gincategories = None
 
     if request.GET:
+        if 'gincategory' in request.GET:
+            gincategories = request.GET['gincategory'].split(',')
+            gins = gins.filter(gincategory__name__in=gincategories)
+            gincategories = GinCategory.objects.filter(name__in=gincategories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -29,6 +35,7 @@ def all_gins(request):
     context = {
         'gins': gins,
         'search_input': query,
+        'current_gincategory': gincategories
     }
 
     return render(request, 'gins/gins.html', context)
