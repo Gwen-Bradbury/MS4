12099aee3d682b0538151e310af6bd5469b041cc
Gwen Bradbury <charmed_89@msn.com>
2021-04-16 09:47:58 +0000

    Added Gins App, Models and Fixtures

---
+++ gins/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Gin, GinCategory
+
+
+admin.site.register(Gin)
+admin.site.register(GinCategory)
---
+++ gins/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class GinCategory(models.Model):
+    name = models.CharField(max_length=150)
+    friendly_name = models.CharField(max_length=150, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Gin(models.Model):
+    gincategory = models.ForeignKey(
+        'GinCategory', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
