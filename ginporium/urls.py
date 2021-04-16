35f01773ad3f98f168a24baed164f93de40c0b3c
Gwen Bradbury <charmed_89@msn.com>
2021-04-16 10:46:00 +0000

    Added Gin Views and Template

--- ginporium/urls.py
+++ ginporium/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('gins/', include('gins.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gins/views.py
+++ gins/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Gin
 
-# Create your views here.
+
+def all_gins(request):
+    """ Shows All Gins, Including Searching and Sorting Queries """
+    gins = Gin.objects.all()
+
+    context = {
+        'gins': gins,
+    }
+
+    return render(request, 'gins/gins.html', context)
