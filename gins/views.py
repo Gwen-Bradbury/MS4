5739ad52b83ac36c9feed44732936d44fb3abba6
Gwen Bradbury <charmed_89@msn.com>
2021-04-17 15:59:51 +0000

    Added JS Sorting and To Top Button

--- gins/views.py
+++ gins/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import (
     )
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Gin, GinCategory
 
 
@@ -14,8 +16,24 @@ def all_gins(request):
     gins = Gin.objects.all()
     query = None
     gincategories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                gins = gins.annotate(lower_name=Lower('name'))
+            if sortkey == 'gincategory':
+                sortkey = 'gincategory__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            gins = gins.order_by(sortkey)
+
         if 'gincategory' in request.GET:
             gincategories = request.GET['gincategory'].split(',')
             gins = gins.filter(gincategory__name__in=gincategories)
@@ -32,10 +50,13 @@ def all_gins(request):
                 description__icontains=query)
             gins = gins.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'gins': gins,
         'search_input': query,
         'current_gincategory': gincategories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'gins/gins.html', context)
