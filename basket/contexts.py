3fad082535e7b687597a052275c164056e01b2ac
Gwen Bradbury <charmed_89@msn.com>
2021-04-18 14:17:59 +0000

    Updated Context Processor

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from gins.models import Gin
 
 
 def basket_items(request):
@@ -7,9 +9,21 @@ def basket_items(request):
     basket_products = []
     total = 0
     item_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        gin = get_object_or_404(Gin, pk=item_id)
+        total += quantity * gin.price
+        item_count += quantity
+        basket_products.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'gin': gin,
+        })
+
     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
 
-    grand_total = delivery * total
+    grand_total = delivery + total
 
     context = {
         'basket_products': basket_products,
