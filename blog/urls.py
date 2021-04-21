0887b3ebd0513bc8c0e55ec410408446ea0c5f07
Gwen Bradbury <charmed_89@msn.com>
2021-04-21 09:16:08 +0000

    Added blog_detail.html and Updated blog.html. Added Urls and Views for blog_detail.html

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_blog, name='view_blog')
+    path('', views.view_blog, name='view_blog'),
+    path('<post_id>', views.blog_detail, name='blog_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 
 def view_blog(request):
     """ Returns blog.html """
-    return render(request, 'blog/blog.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/blog.html', context)
+
+
+def blog_detail(request, post_id):
+    """ Returns blog_detail.html """
+    post = get_object_or_404(Post, pk=post_id)
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'blog/blog_detail.html', context)
