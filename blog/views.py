958a27f4f873d7a0e2edda42324d12a511a820a0
Gwen Bradbury <charmed_89@msn.com>
2021-05-04 18:42:18 +0000

    Restyled Profile

--- blog/views.py
+++ blog/views.py
@@ -56,7 +56,7 @@ def blog_detail(request, post_id):
 @login_required
 def edit_comment(request, comment_id):
     """ Author Access Only """
-    if not request.user.is_request.user:
+    if not request.user.is_superuser or request.user.is_comment.comment_author:
         messages.error(request, 'Sorry, only the comment author can do that')
         return redirect(reverse('home'))
     """ Edit Comment """
@@ -87,7 +87,7 @@ def edit_comment(request, comment_id):
 @login_required
 def delete_comment(request, comment_id):
     """ Author Access Only """
-    if not request.user.is_request.user:
+    if not request.user.is_superuser or request.user.is_comment.comment_author:
         messages.error(request, 'Sorry, only the comment author can do that')
         return redirect(reverse('home'))
     """ Delete Comment """
